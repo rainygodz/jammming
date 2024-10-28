5c626528dad0bbf0d06d74b06a263938410a4e50
rainygodz <120272656+rainygodz@users.noreply.github.com>
2024-10-28 21:40:40 +0400

    Add header and SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
+  const [ search, setSearch ] = useState('');
+
+  const handleInput = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="logo">Jammming</h1>
       </header>
+      <SearchBar handleInput={handleInput} value={search}/>
+      <p>{search}</p>
     </div>
   );
 }
